f30ebf9e990307d456082f34a8d55163cd2afbfb
dmswo106 <dmswo106@naver.com>
2021-05-11 15:01:38 +0900

    jpql 패치조인 공부

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -13,26 +13,40 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("화원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m.username from Team t join t.members m";
-            Integer result = em.createQuery(query, Integer.class)
-                    .getSingleResult();
-            System.out.println("result = " + result);
+            String query = "select t from Team t join fetch t.members m";
+            List<Team> result = em.createQuery(query, Team.class)
+                    .getResultList();
+
+            System.out.println("result = " + result.size());
+//            for (Team team : result) {
+//                System.out.println("team = " + team.getName() + "|members = " + team.getMembers().size());
+//            }
 
             tx.commit();
         } catch (Exception e){
